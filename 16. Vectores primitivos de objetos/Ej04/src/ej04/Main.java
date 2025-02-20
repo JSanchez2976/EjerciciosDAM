f7f00386e89b89599889c6043ecb9d26079dfdd5
Jorge Sanchez <jsanchez.contact29@gmail.com>
2025-02-20 09:46:15 +0100

    Ejercicio completo
    Añadí ordenar por academia, y luego por profesores

--- 16. Vectores primitivos de objetos/Ej04/src/ej04/Main.java
+++ 16. Vectores primitivos de objetos/Ej04/src/ej04/Main.java
@@ -1,5 +1,7 @@
 package ej04;
 
+import java.util.Scanner;
+
 /*
  *  Crear un objeto de tipo centrosDeFormacion,
  Rellenar los datos del objeto
@@ -9,9 +11,12 @@ package ej04;
 academia se imparte.
  Ordenar las academias en orden creciente y por cada academia ordenar los profesores por nombre
  */
+
+// NOTA: Tener en cuenta que falta el control de errores en el ejercicio.
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		// Crear un objeto de tipo centrosDeFormacion
 		CentrosDeFormacion centro = new CentrosDeFormacion();
 
@@ -25,19 +30,75 @@ public class Main {
 		Academia[] academias = centro.getAcademias();
 		boolean primeraVez = true;
 		int nCursosMax = 0;
-		String nombreAcademiaMax="";
+		String nombreAcademiaMax = "";
 		for (int i = 0; i < academias.length; i++) {
 			int nCursos = academias[i].getCursos().length;
 			if (primeraVez) {
 				primeraVez = false;
 				nCursosMax = nCursos;
-				nombreAcademiaMax=academias[i].getNombreAcademia();
+				nombreAcademiaMax = academias[i].getNombreAcademia();
 			} else if (nCursosMax < nCursos) {
 				nCursosMax = nCursos;
-				nombreAcademiaMax=academias[i].getNombreAcademia();
+				nombreAcademiaMax = academias[i].getNombreAcademia();
 			}
 		}
-		System.out.println("La academia "+nombreAcademiaMax+", que tiene "+nCursosMax+" cursos es la que más cursos tiene.");
+		System.out.println("La academia " + nombreAcademiaMax + ", que tiene " + nCursosMax
+				+ " cursos es la que más cursos tiene.");
+
+		/*
+		 * Lee por teclado el nombre de un curso y muestra los profesores que lo
+		 * imparten y en que academia se imparte.
+		 */
+		System.out.print("\nDe que curso quieres ver los profesores? : ");
+		String curso = in.nextLine();
+		// buscar el curso en academias
+		for (int i = 0; i < academias.length; i++) {
+			//entrar en cursos
+			Curso [] cursos = academias[i].getCursos();
+			for (int j = 0; j < cursos.length; j++) {
+				if(cursos[j].getNombre().equalsIgnoreCase(curso)) {
+					Profesor[] profesores = cursos[j].getProfesores();
+					for (int k = 0; k < profesores.length; k++) {
+						System.out.println("Los profesores son: "+profesores[k].getNombre());
+					}
+					System.out.println("La academia en la que se imparte es: "+academias[i].getNombreAcademia());
+				}
+			}
+		}
+		
+		// Ordenar las academias en orden creciente y por cada academia ordenar los profesores por nombre
+		// Academias creciente
+		for (int i = 1; i < academias.length; i++) {
+			for (int j = academias.length-1;j>=i; j--) {
+				if(academias[j].getNombreAcademia().compareTo(academias[j-1].getNombreAcademia())<0) {
+					Academia aux = academias[j];
+					academias[j]=academias[j-1];
+					academias[j-1]=aux;
+				}
+			}
+		}
+		// Profesores por nombre
+		for (int i = 0; i < academias.length; i++) {
+			Curso [] cursos = academias[i].getCursos();
+			// Entrar en cursos
+			for (int j = 0; j < cursos.length; j++) {
+				Profesor[] profesores = cursos[j].getProfesores();
+				// Binaria con profesores
+				for (int k = 1; k < profesores.length; k++) {
+					for (int k2 = profesores.length-1;k2>=k; k2--) {
+						if(profesores[k2].getNombre().compareTo(profesores[k2-1].getNombre())<0) {
+							Profesor aux = profesores[k2];
+							profesores [k2] = profesores[k2-1];
+							profesores[k2-1]=aux;
+						}
+					}
+				}
+			}
+		}
+		
+		// Visualizar todos los datos
+		centro.visualizarCentros();
+		in.close();
 	}
 
 }
